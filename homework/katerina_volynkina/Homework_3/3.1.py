5d57779a35e1cd76246815dabb393739b99ab5f2
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-05-14 20:01:20 +0300

    Tasks

---
+++ homework/katerina_volynkina/Homework_3/3.1.py
@@ -0,0 +1,20 @@
+#Даны 2 числа a и b. Получить их сумму, разность и произведение
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+def minus_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
+
+print(sum_numbers(2, 3))
+print(minus_numbers(2, 3))
+print(multiply_numbers(2, 3))
+print(divide_numbers(2, 3))
+
+
