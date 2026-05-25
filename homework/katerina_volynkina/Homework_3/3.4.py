048d31038fd5eadcf4fd710b460f71171468ce13
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-05-25 12:57:21 +0300

    Homework_3 change_Lint

--- homework/katerina_volynkina/Homework_3/3.4.py
+++ homework/katerina_volynkina/Homework_3/3.4.py
@@ -1,10 +1,13 @@
 # Даны катеты прямоугольного треугольника. Найти его гипотенузу и площадь
+
+
 def hypotenuse(x, y):
+
     return (x**2 + y**2)**0.5
 
 def triangle_area(x, y):
+
     return (x * y) / 2
 
 print(hypotenuse(2, 1))
 print(triangle_area(2, 3))
-
