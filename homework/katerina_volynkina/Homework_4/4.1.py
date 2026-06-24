41a4980b4225137042cdb49cc1a2ce1bfee6ee2e
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-06-24 12:12:28 +0300

    done

---
+++ homework/katerina_volynkina/Homework_4/4.1.py
@@ -0,0 +1,10 @@
+my_dict = {'tuple': (2, 'test', 8, 3.2, [1, 5, 7]), 'list': [45, 87, 26, 34, 12, ['лето', 'жара']],
+           'dict': {'1': 'январь', '2': 'февраль', '3': 'март', '4': 'апрель', '5': 'май'}, 'set': {2, 58, 12, 48 ,75}}
+print(my_dict['tuple'][-1])
+my_dict['list'].append(83)
+my_dict['list'].pop(1)
+my_dict['dict']["('i am a tuple',)"] = '1'
+del my_dict['dict']['5']
+my_dict['set'].add(25)
+my_dict['set'].pop()
+print(my_dict)
