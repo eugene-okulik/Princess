f767110e45b44237da61909fbff2abafeeec323a
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-06-25 18:37:03 +0300

    Done

---
+++ homework/katerina_volynkina/Homework_5/5.3.py
@@ -0,0 +1,4 @@
+student = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+print(f'Students {student[0]}, {student[1]}, {student[2]} '
+      f'study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}')
