b3ee168412744b258420f12bc9a1b7d3d5c4af21
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-06-25 18:43:18 +0300

    Homework_5 change_Lint_2

--- homework/katerina_volynkina/Homework_5/5.3.py
+++ homework/katerina_volynkina/Homework_5/5.3.py
@@ -1,4 +1,5 @@
 student = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
+
 print(f'Students {student[0]}, {student[1]}, {student[2]} '
       f'study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}')
