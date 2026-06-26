aae622271901fd69dcf54bc10900240c7c7480ba
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-06-26 11:02:49 +0300

    Homework_5 fixed

--- homework/katerina_volynkina/Homework_5/5.3.py
+++ homework/katerina_volynkina/Homework_5/5.3.py
@@ -1,5 +1,5 @@
 student = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
-
-print(f'Students {student[0]}, {student[1]}, {student[2]} '
-      f'study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}')
+student = ','.join(student)
+subjects = ','.join(subjects)
+print(f'Students {student} study these subjects: {subjects}')
