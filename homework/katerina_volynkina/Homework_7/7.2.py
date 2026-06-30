fa9bd2146da3956e9e88a257e0e6f44bb00a7f99
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-06-30 21:02:12 +0300

    Homework_7 change_Lint_2

--- homework/katerina_volynkina/Homework_7/7.2.py
+++ homework/katerina_volynkina/Homework_7/7.2.py
@@ -1,8 +1,12 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
+
+
 def count_words(dictionary):
     for key, value in words.items():
-        print (key*value)
+        print(key * value)
+
 
 count_words(words)
+
 
 
--- homework/katerina_volynkina/Homework_7/7.3.py
+++ homework/katerina_volynkina/Homework_7/7.3.py
@@ -3,9 +3,11 @@ text_2 = 'результат операции: 54'
 text_3 = 'результат работы программы: 209'
 text_4 = 'результат: 2'
 
+
 def res(text):
     result = text.split(': ')
     print(int(result[1]) + 10)
+
 
 res(text_1)
 res(text_2)
