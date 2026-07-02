a7b0a3c955dbad633b10c449d691b5516efa9015
Ekaterina Volynkina <katerina.vakutina@gmail.com>
2026-07-02 21:37:21 +0300

    Homework_8_done

---
+++ homework/katerina_volynkina/Homework_8/8.2.py
@@ -0,0 +1,26 @@
+import sys
+
+sys.set_int_max_str_digits(30000)
+
+
+def generator_fibonacci(limit=100):
+    a, b = 0, 1
+    count = 0
+    while count < limit:
+        yield a
+        a, b = b, a + b
+        count += 1
+
+
+data = [5, 200, 1000, 100000]
+count = 1
+
+for i in generator_fibonacci(100001):
+    for j in data:
+        if count == j:
+            print(i)
+            break
+    count += 1
+
+
+
